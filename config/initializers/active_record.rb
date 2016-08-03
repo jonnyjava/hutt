3f18ffbfd2e44398f663288e1cd88afdd3fe002c
andrea carrozzo <jonnyjava.net@gmail.com>
2016-08-03 17:55:44 +0200

    use a local_env file to load sensible data

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -1,5 +1,11 @@
 require 'erb'
+env_file = 'config/local_env.yml'
+YAML.load(File.open(env_file)).each do |key, value|
+  ENV[key.to_s] = value
+end if File.exist?(env_file)
+
 db = YAML.load(ERB.new(File.read('./config/database.yml')).result)[Napa.env]
+
 ActiveRecord::Base.establish_connection(db)
 ActiveRecord::Base.logger = Napa::Logger.logger if Napa.env.development?
 ActiveRecord::Base.include_root_in_json = false
