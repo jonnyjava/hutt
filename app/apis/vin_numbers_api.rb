38ad92176e2a92c4c550211c3929258d7b45e885
jonnyjava <jonnyjava.net@gmail.com>
2016-08-30 23:44:19 +0200

    change js driver from webkit to poltergeist

--- app/apis/vin_numbers_api.rb
+++ app/apis/vin_numbers_api.rb
@@ -1,10 +1,10 @@
 class VinNumbersApi < Grape::API
   params do
-    requires :vin, desc: 'the vin number'
+    requires :vin, type: String, desc: 'the vin number'
   end
   route_param :vin do
-    desc 'Get an vin_number'
-    get do
+    desc 'Decode an vin_number'
+    post do
       vin_number = VinNumber.find_or_create_by_vin_number(permitted_params[:vin])
       represent vin_number, with: VinNumberRepresenter
     end
