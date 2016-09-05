4a9a3379ef27efd2babe2bdc20348c8781dd8893
jonnyjava <jonnyjava.net@gmail.com>
2016-09-06 00:03:42 +0200

    loggin active record

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -9,3 +9,5 @@ db = YAML.load(ERB.new(File.read('./config/database.yml')).result)[Napa.env]
 ActiveRecord::Base.establish_connection(db)
 ActiveRecord::Base.logger = Napa::Logger.logger if Napa.env.development?
 ActiveRecord::Base.include_root_in_json = false
+
+Napa::Logger.logger.debug '----------SONO IN initializer/active_record-----------------------'
