ec08f6d6f5ab00f0b5785adef49653d885453b65
jonnyjava <jonnyjava.net@gmail.com>
2016-09-05 23:33:06 +0200

    some puts

--- app/models/vin_number.rb
+++ app/models/vin_number.rb
@@ -3,11 +3,17 @@ class VinNumber < ActiveRecord::Base
 
   def self.find_or_create_by_vin_number(vin)
     vin_number = VinNumber.where(vin: vin)
+puts "-------------------------1"
     if !vin_number.present?
+puts "-------------------------2"
       vin_number = self.create!(vin: vin)
+puts "-------------------------3"
       properties = self.get_from_outside(vin)
+puts "-------------------------4"
       Property.save_list(vin_number.id, properties)
+puts "-------------------------5"
     end
+puts "-------------------------6"
     vin_number
   end
 
